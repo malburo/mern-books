ab9bcb7fbe8c699ba805eb19ef3fced085d9c756
malburo <tongquocbao0605@gmail.com>
2020-06-09 23:31:10 +0700

    login & logout actions

--- src/features/Auth/AuthSlice.js
+++ src/features/Auth/AuthSlice.js
@@ -1,18 +1,46 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// const initialUser = localStorage.getItem('user')
+//   ? JSON.parse(localStorage.getItem('user'))
+//   : null;
+
 const auth = createSlice({
   name: 'auth',
-  initialState: {},
+  initialState: {
+    isAuthenticated: false,
+    user: {},
+  },
   reducers: {
-    login: (state, action) => {
-      state.login = action.payload;
+    loginSuccess: (state, action) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
     },
-    register: (state, action) => {
-      state.register = action.payload;
+    logoutSuccess: (state, action) => {
+      state.user = null;
+      state.isAuthenticated = false;
     },
   },
 });
 
-const { reducer, actions } = auth;
-export const { login, register } = actions;
-export default reducer;
+export default auth.reducer;
+
+// actions
+const { loginSuccess, logoutSuccess } = auth.actions;
+
+export const login = ({ username, password }) => async dispatch => {
+  try {
+    // await api.post('/api/auth/login/', { username, password })
+    dispatch(loginSuccess({ username }));
+  } catch (e) {
+    return console.error(e.message);
+  }
+};
+
+export const logout = () => async dispatch => {
+  try {
+    // await api.post('/api/auth/logout/')
+    return dispatch(logoutSuccess());
+  } catch (e) {
+    return console.error(e.message);
+  }
+};
